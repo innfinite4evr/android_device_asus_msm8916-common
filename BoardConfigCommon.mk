b852382a004d627f2da9da4d83a3d6298817aa1b
Innfinite4evr <Innfinite4evr@gmail.com>
2017-04-27 12:48:09 +0000

    Screwd: Initial Bringup
    Signed-off-by: Innfinite4evr <Innfinite4evr@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,16 +49,16 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_DTBTOOL_ARGS := -3
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/asus/msm8916
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_HEADER_ARCH := arm64
-
+#BOARD_DTBTOOL_ARGS := -3
+#BOARD_KERNEL_BASE := 0x80000000
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+#BOARD_KERNEL_PAGESIZE := 2048
+#TARGET_KERNEL_SOURCE := kernel/asus/msm8916
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+#TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/Image.gz-dtb
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
