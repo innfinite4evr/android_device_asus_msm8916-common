0520b79926e5411cb7652cb5300c58e488032a76
root <root@spartan.z10smfklbsgudlsriqrivaeaje.cx.internal.cloudapp.net>
2017-04-27 07:50:51 +0000

    Kek.lol
    Signed-off-by: root <root@spartan.z10smfklbsgudlsriqrivaeaje.cx.internal.cloudapp.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,9 @@ VENDOR_PATH := device/asus/msm8916-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
 
+# QSAP_SDK
+#USE_PREBUILT_LIBQSAP_SDK := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
