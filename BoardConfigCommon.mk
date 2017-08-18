1509f6b6956afff97c0a3d55ae88b39c467b0645
innfinite4evr <innfinite4evr@gmail.com>
2017-08-18 17:44:14 +0530

    Remove Dexpropt from common tree.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,16 +91,6 @@ BOARD_HARDWARE_CLASS += \
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
